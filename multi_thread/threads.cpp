beb91294539c49c6a1b22ac77f22800869fc4e96
wqu <wqu@vmware.com>
2017-05-01 16:39:44 +0800

    add a thread file

---
+++ multi_thread/threads.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<unistd.h>
+#include<pthread.h>
+using namespace std;
+
+void *thread(void *ptr)
+{
+	for(int i=0;i<3;i++) {
+		sleep(1);
+		cout<<"this is a thread process"<<endl;
+	}
+	return 0;
+}
+
+int main()
+{
+	pthread_t id;
+	int ret = pthread_create(&id, NULL, thread, NULL);
+	if(ret) {
+		cout<<"Failed to create thread"<<endl;
+		return 1;
+	}
+	for(int i=0;i<3;i++) {
+		cout<<"this is a main process"<<endl;
+		sleep(1);
+	}
+	pthread_join(id, NULL);
+	return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+using namespace std;
+
+void reverse(char *begin, char *end)
+{
+	while(begin < end) {
+		char tmp = *begin;
+		*begin = *end;
+		*end = tmp;
+		begin++;
+		end--;
+	}
+	return;
+}
+
+void reverseSen(char *sen)
+{
+	char *begin = sen;
+	char *end = sen;
+	while(*end != '\0') {
+		end++;
+	}
+	reverse(begin, --end);
+	end = sen;
+	while(*begin != '\0') {
+		if(*begin == ' ') {
+			begin++;
+			end++;
+		} else		if(*end == ' ' || *end == '\0') {
+			reverse(begin, --end);
+			begin = ++end;
+		} else {
+			end++;
+		}
+	}
+	return;
+}
 
 int main()
 {
-	std::cout<<"print hello world"<<std::endl;
+	char sen[20] = "this is a sen.";
+	reverseSen(sen);
+	cout<<sen<<endl;
 }
+	
