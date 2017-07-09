f70ebae3d12c7651935177a7fc0b8f61dcb8eb30
steveatgit <quwei0930@163.com>
2017-07-08 22:51:09 -0400

    add socket files

--- socket/tinyhttpd/simpleclient.c
+++ socket/tinyhttpd/simpleclient.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
  sockfd = socket(AF_INET, SOCK_STREAM, 0);
  address.sin_family = AF_INET;
  address.sin_addr.s_addr = inet_addr("127.0.0.1");
- address.sin_port = htons(9734);
+ address.sin_port = htons(49840);
  len = sizeof(address);
  result = connect(sockfd, (struct sockaddr *)&address, len);
 
